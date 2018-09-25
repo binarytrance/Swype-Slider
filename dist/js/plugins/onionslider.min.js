cecf69ffbc6e8ed52ea5b8cd34d93d2041392295
binarytrance <ganeshan.dash@gmail.com>
2018-09-25 13:51:52 +0530

    removed jquery code comments

--- dist/js/plugins/onionslider.min.js
+++ dist/js/plugins/onionslider.min.js
@@ -52,14 +52,7 @@
 
 	// private method that build the slider initially
 	buildSlider = () => {
-		console.log(temp)
-		// number of slider
-		// var slideNumber = 0;
-		// number of clicks required to see entire slider slides
-		// var clicks = 0;
-		// number of clicks required
-		// var clickNumber = slideNumber - 3;
-		
+		console.log(temp)		
 		// calculates the width of slides based on the number of slides that are to be visible
 		this.slideWidth = Math.round((100 / this.visibleSlides) * 100) / 100;
 		console.log(this.swypeSliderWrapper);
@@ -175,82 +168,8 @@
 		}
 		return source;
 	}
-	// slider initialisation
-	// var translateVal = 0;
-	// // number of slider
-	// var slideNumber = 0;
-	// // number of clicks required to see entire slider slides
-	// var clicks = 0;
-	// // number of clicks required
-	// var clickNumber = slideNumber - 3;
-	// // number of slides visible on screen at any given time
-	// var visibleSlides = 3;
-	// // calculates the width of slides based on the number of slides that are to be visible
-	// var slideWidth = Math.round((100 / visibleSlides) * 100) / 100;
-	// console.log(slideWidth);
-	// var slidVal = 0;
-	// $(".swypeSlide").css("width", slideWidth + "%");
-	/// converted to js ==================================================================================================
-	// console.log(slideNumber);
-	//var maxTranslation = visibleSlides * slideWidth;
-	// var maxTranslation = 0;
-	// $(".swypeSlider-wrapper").attr("data-translate", 0);
-	// var clickedParent = $(this).siblings(".slider-gran").children(".slider-parent");
 
-	// slider arrow click events
-	// $(".js-round-arrow").on("click", function() {
-	// 	console.log($(this));
-	// 	// initialise the number of slides on the clicked panel
-	// 	slideNumber = $(this).siblings($(".swypeSlider-container")).children(".swypeSlider-wrapper").children(".swypeSlide").length;
-	// 	clicks = slideNumber - visibleSlides;
-	// 	maxTranslation = clicks * slideWidth;
-	// 	var maxTranslationRounded = Math.round(maxTranslation * 100) / 100;
-	// 	// console.log( slideNumber, slideWidth, maxTranslation);
-	// 	// if($(this).hasClass("round-arrow--left") && $(window).width() > 769) {
-	// 	// 	// console.log("left clicked");
-	// 	// 	var attrVal = $(this).siblings(".slider-gran").children(".slider-parent").attr("data-translate");
-	// 	// 	// console.log(attrVal);
-	// 	// 	var newAttrVal = parseFloat(attrVal)+slideWidth;
-	// 	// 	translateVal = newAttrVal;
-	// 	// 	var translateValRounded = Math.round(translateVal * 100) / 100;
-	// 	// 	$(this).siblings(".slider-gran").children(".slider-parent").attr("data-translate", translateVal);
-	// 	// }
-	// 	// else if($(this).hasClass("round-arrow--right") && $(window).width() > 769) {
-	// 	// 	// console.log("right clicked");
-	// 	// 	var attrVal = $(this).siblings(".slider-gran").children(".slider-parent").attr("data-translate");
-	// 	// 	// console.log(attrVal);
-	// 	// 	var newAttrVal = parseFloat(attrVal)+ (-slideWidth);
-	// 	// 	translateVal = newAttrVal;
-	// 	// 	var translateValRounded = Math.round(translateVal * 100) / 100;
-	// 	// 	$(this).siblings(".slider-gran").children(".slider-parent").attr("data-translate", translateVal);
-	// 	// }
-	// 	// console.log(translateVal, maxTranslationRounded);
-
-
-	// 	if(translateValRounded === 0) {
-	// 		if($(this).hasClass("round-arrow--left")) {
-	// 			$(this).css("display", "none")
-	// 		}
-	// 		// $(".round-arrow--left").css("display", "none");
-	// 	}
-	// 	else {
-	// 		$(this).siblings(".round-arrow--left").css("display", "flex");
-	// 	}
-	// 	if(translateValRounded <= -maxTranslationRounded) {
-	// 		if($(this).hasClass("round-arrow--right")) {
-	// 			$(this).css("display", "none")
-	// 		}
-	// 		// $(".round-arrow--right").css("display", "none");
-	// 	}
-	// 	else {
-	// 		$(this).siblings(".round-arrow--right").css("display", "flex");
-	// 	}
-	// 	// console.log($(this).siblings(".slider-gran").children(".slider-parent"));
-	// 	// $(this).siblings().children(".slider-parent");
-	// 	// $(this).siblings(".slider-gran").children(".slider-parent").attr("data-translate", translateVal);
-	// 	// $(this).siblings(".slider-gran").children(".slider-parent").css("transform", "translateX(" + translateVal + "%)");
-	// });
-
+	// hasClass function to see if the target element has the particular class or not
 	function hasClass(element, className) {
 		return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
 	}
